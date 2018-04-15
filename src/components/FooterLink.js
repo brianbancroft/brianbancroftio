b6bd9b8cfacc83d261adbe423f9c8fef540fc006
Brian Bancroft <hello@brianbancroft.ca>
2018-04-14 22:34:39 -0400

    ✨  Icons for contacts
    Unable to remove highlights. Might ask for help

--- src/components/FooterLink.js
+++ src/components/FooterLink.js
@@ -7,7 +7,7 @@ import IconContainer from './IconContainer'
 const TextContainer = styled.div`
   text-transform: uppercase;
   font-size: 12px;
-  color: black;
+  color: #222;
 `
 
 class Footer extends React.Component {
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,20 +3,32 @@ import Link from 'gatsby-link'
 import { Container } from 'react-responsive-grid'
 
 
+import {FaGithubSquare, FaLinkedinSquare, FaTwitterSquare, FaEnvelopeSquare} from 'react-icons/lib/fa'
+
+
 class Contact extends React.Component {
   render() {
     return (
       <div>
         <h2>Get in Touch!</h2>
         <p>Feel free to get in touch any time you have a question or problem that needs solving</p>
-
-        <h3>hello@brianbancroft.ca</h3>
-        <ul>
-          <li><a href="https://ca.linkedin.com/geobrian">Linkedin</a></li>
-          <li><a href="https://twitter.com/brian_bancroft">Twitter</a></li>
-          <li><a href="https://github.com/brianbancroft">Github</a></li>
-          <li><a href="https://mastodon.club/@brianbancroft">Mastodon</a></li>
-        </ul>
+          <div className="contacts-container">
+          <div className="contacts-container__contact">
+            <a href="https://github.com/brianbancroft" target="_blank"><FaGithubSquare size={56}/></a>
+          </div>
+          <div className="contacts-container__contact">
+            <a href="https://ca.linkedin.com/geobrian" target="_blank"><FaLinkedinSquare size={56}/></a>
+          </div>
+          <div className="contacts-container__contact">
+            <a href="https://twitter.com/brian_bancroft" target="_blank"><FaTwitterSquare size={56}/></a>
+          </div>
+          <div className="contacts-container__contact">
+            <a href="mailto:hello@brianbancroft.ca" target="_blank"><FaEnvelopeSquare size={56}/></a>
+          </div>
+          {/* <div className="contacts-container__contact">
+            <a href="mailto:hello@brianbancroft.ca" target="_blank">Test</a>
+          </div> */}
+        </div>
       </div>
     )
   }
