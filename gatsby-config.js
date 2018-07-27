5049c1181fb6b22cea774a0e43a97c933e739c4d
Brian Bancroft <hello@brianbancroft.ca>
2018-07-26 21:51:20 -0400

    ✨  Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,5 +56,19 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-119666699-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
