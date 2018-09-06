d30baa7c35512f6aba4fcd055c919e007a651de3
Brian Bancroft <hello@brianbancroft.ca>
2018-09-05 23:59:43 -0400

    More cleaning

--- src/components/hexagon-container.js
+++ src/components/hexagon-container.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import Hexagon from './Hexagon'
+import { Hexagon } from '.'
 
 class HexagonContainer extends React.Component {
   render() {
     return (
-      <li
-        key={this.props.id}
-        onClick={this.props.clickAction}
-      >
+      <li key={this.props.id} onClick={this.props.clickAction}>
         <Hexagon
           title={this.props.title}
           backgroundImage={this.props.backgroundImage}
