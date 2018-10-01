a2c668ed7917cb644d9e595f3dbc511391fa2436
Brian Bancroft <hello@brianbancroft.ca>
2018-09-30 21:00:31 -0400

    📝  Update contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,13 +20,15 @@ export default () => {
       <Helmet title="Get in Touch | Brian Bancroft" />
       <h2>Get in Touch!</h2>
       <p>
-        While I'm hapilly-employed over at @EQWorks, I'm always looking for
-        problems to solve on the side. If you want to get in touch, these are
-        the best ways to get a hold of me. Also, my general resume is also
+        I'm always looking for problems to solve on the side, and am able to
+        take on short contracts. If you want to get in touch, these are the best
+        ways to get a hold of me. If you want it, my general resume is also
         available{' '}
         <a href="https://cdn.brianbancroft.io/assets/resumes/2018/resume_brian_bancroft_2018.pdf">
           here
-        </a>.
+        </a>. My skills are mainly in GIS analysis, mapmaking, and fullstack
+        software development. While I slink away from PHP, I'm happy to work
+        with most other stacks.
       </p>
       <div className={contactsContainerStyle}>{linkList}</div>
     </div>
