6ec80cd5d521f4baef62f858a2b7927d6edbed8a
Brian Bancroft <hello@brianbancroft.ca>
2018-10-01 17:10:22 -0400

    💄  Add bottom  margin

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -69,6 +69,7 @@ class Template extends React.Component {
           style={{
             maxWidth: rhythm(24),
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            marginBottom: '90px',
           }}
         >
           {header}
